ac1d4c2ebc7821090753d865fef93c6c09290108
jominsong <kai42tama1012@gmail.com>
2025-07-13 22:30:08 +0900

    로프 물리 수정

--- Dream-Catcher/Assets/Simple 2D Platformer BE2/Scripts/GrapplingHook.cs
+++ Dream-Catcher/Assets/Simple 2D Platformer BE2/Scripts/GrapplingHook.cs
@@ -7,35 +7,42 @@ public class GrapplingHook : MonoBehaviour
     public bool isHookActivate;
     public bool isLineMax;
     public bool isAttach;
-    Vector2 mousedir;
+
+    Vector2 hookDir;
+    Transform targetRing;
+
     void Start()
     {
         line.positionCount = 2;
         line.endWidth = line.startWidth = 0.05f;
-        line.SetPosition(0,transform.position);
-        line.SetPosition(1,hook.position);
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, hook.position);
         line.useWorldSpace = true;
         isAttach = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         line.SetPosition(0, transform.position);
         line.SetPosition(1, hook.position);
-        
+
         if (Input.GetKeyUp(KeyCode.E) && !isHookActivate)
         {
-            hook.position = transform.position;
-            mousedir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            isHookActivate=true;
-            isLineMax = false;
-            hook.gameObject.SetActive(true);
+            targetRing = FindNearestRing();
+
+            if (targetRing != null)
+            {
+                hook.position = transform.position;
+                hookDir = ((Vector2)targetRing.position - (Vector2)transform.position).normalized;
+                isHookActivate = true;
+                isLineMax = false;
+                hook.gameObject.SetActive(true);
+            }
         }
 
         if (isHookActivate && !isLineMax && !isAttach)
         {
-            hook.Translate(mousedir.normalized * Time.deltaTime * 15);
+            hook.Translate(hookDir * Time.deltaTime * 15);
 
             if (Vector2.Distance(transform.position, hook.position) > 5)
             {
@@ -45,7 +52,7 @@ public class GrapplingHook : MonoBehaviour
         else if (isHookActivate && isLineMax && !isAttach)
         {
             hook.position = Vector2.MoveTowards(hook.position, transform.position, Time.deltaTime * 15);
-            if(Vector2.Distance(transform.position, hook.position) < 0.1f)
+            if (Vector2.Distance(transform.position, hook.position) < 0.1f)
             {
                 isHookActivate = false;
                 isLineMax = false;
@@ -59,9 +66,29 @@ public class GrapplingHook : MonoBehaviour
                 isAttach = false;
                 isHookActivate = false;
                 isLineMax = false;
-                hook.GetComponent<Hooking>().joint2D.enabled = false;
+                hook.GetComponent<Hooking>().springJoint.enabled = false;
                 hook.gameObject.SetActive(false);
             }
         }
+    }
+
+    // 가장 가까운 Ring 오브젝트 찾기
+    Transform FindNearestRing()
+    {
+        GameObject[] rings = GameObject.FindGameObjectsWithTag("Ring");
+        Transform nearest = null;
+        float minDist = Mathf.Infinity;
+
+        foreach (GameObject ring in rings)
+        {
+            float dist = Vector2.Distance(transform.position, ring.transform.position);
+            if (dist < minDist)
+            {
+                minDist = dist;
+                nearest = ring.transform;
+            }
+        }
+
+        return nearest;
     }
 }
